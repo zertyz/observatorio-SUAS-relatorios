6abf618debcbc5bab9985c1741a16a40ae3f190a
tais <tais.salles93@yahoo.com>
2017-02-17 10:04:04 -0200

    Criação da tela2 - Dados dos municípios. Obs: Incompleta

---
+++ src/app/pages/tela2/tela2.component.ts
@@ -0,0 +1,13 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'tela2',
+  styles: ['./tela2.scss'],
+  templateUrl: 'tela2.html'
+})
+export class Tela2 {
+
+  constructor() {
+  }
+
+}
---
+++ src/app/pages/tela2/tela2.routing.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule }  from '@angular/router';
+
+import { Tela2 } from './tela2.component';
+import { ModuleWithProviders } from '@angular/core';
+
+// noinspection TypeScriptValidateTypes
+const routes: Routes = [
+  {
+    path: '',
+    component: Tela2,
+    children: [
+      //{ path: 'treeview', component: TreeViewComponent }
+    ]
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forChild(routes);
