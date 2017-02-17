3382fe25e6cae878223b89282bb12ff5e7028104
tais <tais.salles93@yahoo.com>
2017-02-17 10:04:04 -0200

    Criação da tela2 - Dados dos municípios. Obs: Incompleta

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -16,16 +16,16 @@ import { PAGES_MENU } from './pages.menu';
       </div>
     </div>
     <footer class="al-footer clearfix">
-      <div class="al-footer-right">Created with <i class="ion-heart"></i></div>
+      <div class="al-footer-right"> 2017 &copy; Todos os direitos reservados </div>
       <div class="al-footer-main clearfix">
-        <div class="al-copy"><a href="http://www.mprj.mp.br/"> MPRJ - MINISTÉRIO PÚBLICO DO ESTADO DO RIO DE JANEIRO</a> 2017
-         &copy; Todos os direitos reservados</div>
         <ul class="al-share clearfix">
           <li><a href="" target="_blank"><i class="socicon socicon-facebook"></i></a></li>
           <li><a href="" target="_blank"><i class="socicon socicon-twitter"></i></a></li>
-          <li><a href="" target="_blank"><i class="socicon socicon-google"></i></a>/li>
+          <li><a href="" target="_blank"><i class="socicon socicon-google"></i></a></li>
           <li><a href="" target="_blank"><i class="socicon socicon-github"></i></a></li>
         </ul>
+        
+        <div class="al-copy"> <a href="http://www.mprj.mp.br/"> MPRJ - MINISTÉRIO PÚBLICO DO ESTADO DO RIO DE JANEIRO</a> </div>
       </div>
     </footer>
     <ba-back-top position="200"></ba-back-top>
